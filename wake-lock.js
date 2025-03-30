21d7fcc7877da69966825ec84457d67a35be8f34
Todd Bichoupan <tbcatan@gmail.com>
2025-03-30 19:39:17 -0400

    visibility check

--- wake-lock.js
+++ wake-lock.js
@@ -1,7 +1,9 @@
 let wakeLock;
-navigator.wakeLock.request("screen").then((wl) => {
-  wakeLock = wl;
-});
+if (document.visibilityState === "visible") {
+  navigator.wakeLock.request("screen").then((wl) => {
+    wakeLock = wl;
+  });
+}
 document.addEventListener("visibilitychange", async () => {
   if (document.visibilityState === "visible" && (wakeLock == null || wakeLock.released)) {
     wakeLock = await navigator.wakeLock.request("screen");
