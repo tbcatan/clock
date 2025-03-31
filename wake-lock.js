692a50e04f90d69a48966a55780d8cfdb1806c3a
Todd Bichoupan <tbcatan@gmail.com>
2025-03-30 20:08:48 -0400

    wake lock on click

--- wake-lock.js
+++ wake-lock.js
@@ -1,10 +1,15 @@
 let wakeLock;
 if (document.visibilityState === "visible") {
-  navigator.wakeLock.request("screen").then((wl) => {
-    wakeLock = wl;
+  navigator.wakeLock.request("screen").then((screenWakeLock) => {
+    wakeLock = screenWakeLock;
   });
 }
 document.addEventListener("visibilitychange", async () => {
+  if (document.visibilityState === "visible" && (wakeLock == null || wakeLock.released)) {
+    wakeLock = await navigator.wakeLock.request("screen");
+  }
+});
+document.addEventListener("click", async () => {
   if (document.visibilityState === "visible" && (wakeLock == null || wakeLock.released)) {
     wakeLock = await navigator.wakeLock.request("screen");
   }
