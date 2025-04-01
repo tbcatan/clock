3b25e4135f521a45ee678334f8cef2f0b4a34629
Todd Bichoupan <tbcatan@gmail.com>
2025-04-01 02:53:49 -0400

    functions

--- edit.js
+++ edit.js
@@ -1,16 +1,16 @@
-function openEditMenu(modifyEditMenu) {
+const openEditMenu = (modifyEditMenu) => {
   const createClock = document.querySelector("#create-clock").content.cloneNode(true);
   modifyEditMenu?.(createClock);
   document.querySelector("#content").classList.add("hidden");
   document.querySelector("#edit").replaceChildren(createClock);
-}
+};
 
-function stopEditing() {
+const stopEditing = () => {
   document.querySelector("#content").classList.remove("hidden");
   document.querySelector("#edit").replaceChildren();
-}
+};
 
-function getNewClockState(playerString, timeMinutes) {
+const getNewClockState = (playerString, timeMinutes) => {
   const players = playerString
     .split(/[\n,;]/)
     .map((name) => name.trim())
@@ -28,13 +28,13 @@ function getNewClockState(playerString, timeMinutes) {
     timestamp: Date.now(),
     clocks: players.map((name) => ({ name, time: timeMilliseconds })),
   };
-}
+};
 
-function createNewClock(publishClockState) {
+const createNewClock = (publishClockState) => {
   const playerInput = document.querySelector("#player-input");
   const timeInput = document.querySelector("#time-input");
   const newClockState = getNewClockState(playerInput.innerText, timeInput.value);
   if (newClockState) {
     publishClockState(newClockState).then(stopEditing);
   }
-}
+};
