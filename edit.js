88ed024a255210ceb4147e0242b7c535559d2cbd
Todd Bichoupan <tbcatan@gmail.com>
2025-04-01 02:11:40 -0400

    creating clocks

---
+++ edit.js
@@ -0,0 +1,39 @@
+function openEditMenu() {
+  document.querySelector("#content").classList.add("hidden");
+  document.querySelector("#edit").replaceChildren(document.querySelector("#create-clock").content.cloneNode(true));
+}
+
+function stopEditing() {
+  document.querySelector("#content").classList.remove("hidden");
+  document.querySelector("#edit").replaceChildren();
+}
+
+function getNewClockState(playerString, timeMinutes) {
+  const players = playerString
+    .split(/[\n,;]/)
+    .map((name) => name.trim())
+    .filter((name) => name)
+    .map((name) => name.replace(/\s+/g, " "));
+  if (players.length === 0) {
+    return;
+  }
+  timeMinutes = timeMinutes.trim();
+  if (!/^\d{1,11}$/.test(timeMinutes)) {
+    return;
+  }
+  const timeMilliseconds = Number(timeMinutes) * 60 * 1000;
+  return {
+    timestamp: Date.now(),
+    clocks: players.map((name) => ({ name, time: timeMilliseconds })),
+  };
+}
+
+function createNewClock(publishClockState) {
+  const playerInput = document.querySelector("#player-input");
+  const timeInput = document.querySelector("#time-input");
+  const newClockState = getNewClockState(playerInput.innerText, timeInput.value);
+  if (newClockState) {
+    publishClockState(newClockState);
+    stopEditing();
+  }
+}
