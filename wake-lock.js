42d561c1d0a8d60edf129b3e23998f8388aaa81f
Todd Bichoupan <tbcatan@gmail.com>
2025-03-31 23:47:09 -0400

    reacquire

--- wake-lock.js
+++ wake-lock.js
@@ -1,9 +1,9 @@
 let wakeLock;
-const acquireWakeLock = async () => {
+const reacquireWakeLock = async () => {
   if (document.visibilityState === "visible" && (wakeLock == null || wakeLock.released)) {
     wakeLock = await navigator.wakeLock.request("screen");
   }
 };
-acquireWakeLock();
-document.addEventListener("visibilitychange", acquireWakeLock);
-document.addEventListener("click", acquireWakeLock);
+reacquireWakeLock();
+document.addEventListener("visibilitychange", reacquireWakeLock);
+document.addEventListener("click", reacquireWakeLock);
