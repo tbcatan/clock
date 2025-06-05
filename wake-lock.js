7a16d5df2b31b24db351814c03b4034846afa2cf
Todd Bichoupan <tbcatan@gmail.com>
2025-06-05 17:43:11 -0400

    screen lock icon

--- wake-lock.js
+++ wake-lock.js
@@ -4,6 +4,25 @@ const reacquireWakeLock = async () => {
     wakeLock = await navigator.wakeLock?.request("screen");
   }
 };
-reacquireWakeLock();
-document.addEventListener("visibilitychange", reacquireWakeLock);
-document.addEventListener("click", reacquireWakeLock);
+
+let isScreenLocked = false;
+const updateScreenLockIcon = () => {
+  const isScreenLockedNow = wakeLock != null && !wakeLock.released;
+  if (isScreenLockedNow === isScreenLocked) {
+    return;
+  }
+  if (isScreenLockedNow) {
+    element("screen-unlocked").classList.add("hidden");
+    element("screen-locked").classList.remove("hidden");
+  } else {
+    element("screen-locked").classList.add("hidden");
+    element("screen-unlocked").classList.remove("hidden");
+  }
+  isScreenLocked = isScreenLockedNow;
+};
+
+const checkWakeLock = () => reacquireWakeLock().finally(updateScreenLockIcon);
+
+checkWakeLock();
+document.addEventListener("visibilitychange", checkWakeLock);
+document.addEventListener("click", checkWakeLock);
